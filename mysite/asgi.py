ee5061759377312a026ed2febd07416db17842a7
Kapil Cadana <kb@cadana.dk>
2025-06-01 15:45:43 +0530

    websocket setup

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,16 +9,34 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-# application = get_asgi_application()
+# # application = get_asgi_application()
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http": get_asgi_application(),
+#         # Just HTTP for now. (We can add other protocols later.)
+#     }
+# )
+
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
